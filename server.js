dbe78c2735eaadf2921cb60750836fcb9bcb8e0a
strkng <bell1221viv@gmail.com>
2026-05-16 19:32:12 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Chat server running");
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data);
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("server started");
+});
